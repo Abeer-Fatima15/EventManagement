6248f7bbd03d02dcb71bba9cb0b749de63770486
Abeer-Fatima15 <157732121+Abeer-Fatima15@users.noreply.github.com>
2024-12-26 11:02:08 +0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,26 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
-import './index.css';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-// Create a router with the future flag
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <App />,
-    },
-  ],
-  {
-    future: {
-      v7_startTransition: true,
-    },
-  }
-);
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
-);
+)
